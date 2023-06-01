88735a8eb8ecf534185aec97befdb934bf54355d
Alen Lović <alenlovic1987@gmail.com>
2023-06-01 17:12:35 +0200

    dummy console app

---
+++ HelloWorld/App.cs
@@ -0,0 +1,31 @@
+﻿using HelloWorldLibrary.BusinessLogic;
+
+namespace HelloWorld;
+
+public class App
+{
+    private readonly IMessages _messages;
+
+    public App(IMessages messages)
+    {
+        _messages = messages;
+    }
+    //HelloWorld.exe -lang=es test
+    public void Run(string[] args)
+    {
+        string lang = "en";
+
+        for (int i = 0; i < args.Length; i++)
+        {
+            if (args[i].ToLower().StartsWith("lang="))
+            {
+                lang = args[i].Substring(5);
+                break;
+            }
+        }
+
+        string message = _messages.Greeting(lang);
+
+        Console.WriteLine(message);
+    }
+}
